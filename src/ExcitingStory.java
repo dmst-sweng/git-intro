0adc9bc94108776f4cdccf1c06bd47c2c2829da5
makyr90 <makyr90@gmail.com>
2014-03-21 00:58:20 +0200

    Continuation of the Story and Insertion of Some Whille loops in order to force Users to give the correct inputs each time.

--- src/ExcitingStory.java
+++ src/ExcitingStory.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
  * A class to print a story that will be developed collaboratively.
- * Extend test thisssssssss test class by adding your literary input near the end of
+ * Extend test this test class by adding your literary input near the end of
  * the main method.  For more marks, you can extend the
  * <a href="#sentence">sentence</a> method.
  */
@@ -22,42 +22,14 @@ public class ExcitingStory {
 		System.out.println(s);
     }
 
-	/**
-	 * Tells a story , different every time , according to the reader's mood.
-	 *
-	 * @param mood A String value that indicates the type of the story we are going to tell.
-	 * @param newLine A String that contains the new line separator.
-	 *
-	 * @return A thankfull message to the reader for his patience.
-	 */
-    static String tellStory(String mood , String newLine) {
 
-		if (mood.equals("-a")) {
-			tellHappyStory(newLine);
-		} 
-		
-		else if (mood.equals("-b")) {
-			tellHorrifyingStory(newLine);
-		}
-		
-		else if (mood.equals("-c")) {
-			tellFunnyStory(newLine);
-		}
-			
-		else {
-			sentence( "You can chose -a or -b or -c , nothing else. Please retry.");
-		}
-		
-		return("Thank you for your help");
-	}
-	
 	/**
 	 * Function that is used to create a horryfying story.
 	 */
 	static void tellHorrifyingStory(String newLine) {
 		sentence("Good for you, cause weather forecast shows that tonight we'll have an equal experience :) .");
 	}
-	
+
 	/**
 	 * Function that is used to create a happy story.
 	 */
@@ -65,54 +37,103 @@ public class ExcitingStory {
 		sentence("Our proposal is to take a day-off and relax, everything is ok!!!");
 		sentence("It doesn't matter if there is a storm outside , you can still sit next to the fireplace and enjoy your hot chocolate.");
 	}
-	
+
 	/**
-	 * Function that is used to create a funny story.
+	 * Function that is used to create a Adventure story.
 	 */
-	static void tellFunnyStory(String newLine) {
+	static void tellAdventureStory(String newLine) {
 		sentence("Suddenly a wolf comes out of the wood" + newLine + "How do you react??" + newLine + "-d.Run"+ newLine +"-f.Fight"+ newLine +"-g.Call your mum(mummas little boy) ");
-		
+
 		Scanner scanner = new Scanner(System.in);
 		String wolf = scanner.nextLine();
+		 while (!(wolf.equals("-d") || wolf.equals("-f") || wolf.equals("-g")) ) {
+			    sentence("You can only Type -d or -f or -g"+newLine+ "Please Try Again");
+		          wolf=scanner.nextLine();
+ }
 		reactionToWolfAppearance(wolf);
 	}
-	
+
 	/**
 	 *	This function is used to inform the reader about the outcome of his choice to the wolf appearance.
 	 *
 	 *	@param answer The answer of the reader.
 	 */
 	static void reactionToWolfAppearance(String answer) {
-		
+     String newLine2 = System.getProperty("line.separator");
 		if(answer.equals("-d")) {
 			String mergeContinuationOfStory="The wolf is faster you die";
 		    mergeContinuationOfStory+="\n You wake up and realise it was just a dream ,within a dream ,within a dream...\n so you decide to call DiCaprio to figure out what's going on.";
 		    sentence(mergeContinuationOfStory);
 	    }
-		
+
 		else if(answer.equals("-f")) {
-			sentence("The wolf is stronger you die");
-		}
-		
+			Scanner scan = new Scanner(System.in);
+			sentence("Choose a weapon from your Bag:"+ newLine2 + "For AK47, choose -h" + newLine2 + "For beretta 92fs, choose -i" + newLine2 + "For Magnum 357, choose -j");
+
+			String weapon=scan.nextLine();
+
+			/* A loop which force the user to insert the correct character (-h or -i or -j) */
+			 while (!(weapon.equals("-h") || weapon.equals("-i") || weapon.equals("-j")) ) {
+				    sentence("You can only Type -h or -i or -j"+newLine2+ "Please Try Again");
+			          weapon=scan.nextLine();
+			 }
+
+					if (weapon.equals("-h")) {
+						     sentence("You kill the wolf by shooting on his head!! Congratulations!! You will survive!! ");
+
+							}
+
+							else if (weapon.equals("-i")) {
+								 sentence("Oh Noooo!! You just run out of bullets!! But lucky you, the wolf get feared and leave running! ");
+							}
+
+							else if (weapon.equals("-j")) {
+								 sentence("You can't shoot the wolf due to a gun engagement.......");
+					}
+			    }
+
+
+
+
+
+
 		else if(answer.equals("-g")) {
 			sentence("Your mum comes out of the woods kicks the wolf and saves you!! You lucky scum.");
 			sentence("But, although you survived, a big brown bear is approaching you on your way home!You have to do something!");
 		}
-		
-		else {
-			sentence( "You can choose -d or -f or -g , nothing else. Please retry.");
-		}
+
+
 	}
 
     public static void main(String[] args) {
 
 		String newLine = System.getProperty("line.separator");
-		sentence("It was a dark and stormy night.");
-		sentence("How was your stormy night?" + newLine + "If you were terrified choose -a" + newLine + "If you enjoyed it, choose -b" + newLine + "If you want to laugh, choose -c");
-
 		Scanner scanner = new Scanner(System.in);
-		String m = scanner.nextLine();
-		sentence(tellStory(m , newLine));
+		sentence("It was a dark and stormy night.");
+		sentence( newLine +"If you were terrified choose -a" + newLine + "If you enjoyed it, choose -b" + newLine + "If you want adventure, choose -c");
+
+/* Initial choice of the reader among Happy, Horryfying and Adventure Story */
+
+String m=scanner.nextLine();
+
+/* A loop which force the user to insert the correct character (-a or -b or -c) */
+ while (!(m.equals("-a") || m.equals("-b") || m.equals("-c")) ) {
+	    sentence("You can only Type -a or -b or -c"+newLine+ "Please Try Again");
+          m=scanner.nextLine();
+ }
+
+		if (m.equals("-a")) {
+			      tellHorrifyingStory(newLine);
+
+				}
+
+				else if (m.equals("-b")) {
+					tellHappyStory(newLine);
+				}
+
+				else if (m.equals("-c")) {
+					tellAdventureStory(newLine);
+		}
     }
-	
+
 }
