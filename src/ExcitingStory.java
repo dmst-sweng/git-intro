044b7138d86f21a364838331b85954d39331346a
Costaspap <costaspapastamos@hotmail.com>
2014-03-26 05:25:51 +0200

    a creepy story

--- src/ExcitingStory.java
+++ src/ExcitingStory.java
@@ -97,6 +97,176 @@ public class ExcitingStory {
 			sentence("Great choice! It was your annoying neighboor. He would probably wanted some sugar... for the 25th time this week.");
 	}
 
+           /**
+		   	 * Function that is used to create a Creepy story.
+		   	 */
+		       static void tellCreepyStory(String newLine) {
+		           sentence("You are sleeping after a hard day when suddenly you wake up."
+		           + newLine + "The storm is raging out of your window and the room is dark and silent.\nEverything seems normal but you feel that something is odd."
+		           + newLine + "With a small move you open your lamp and you get out of your bed.\nAfter scanning the room carefully you decide to drink some water to calm down."
+		           + newLine +"You walk towards the door when suddenly it closes with a shrieking sound.\nThe light flickers and goes off."
+		           + newLine +"Ok,now you're scared!How do you react?"
+		           + newLine +"-a \tGo back to sleep.\nYou are tired enough to get worried about some noises and a little breeze!"
+		           + newLine +"-b \tYou can't just ignore everything,\nyou try to open the door so you can investigate further."
+		           + newLine +"-c \tYou stay still,frozen with fear hoping that it was nothing.");
+
+		           Scanner scanner = new Scanner(System.in);
+		   		String Reaction1 = scanner.nextLine();
+
+		   		while (!(Reaction1.equals("-a") || Reaction1.equals("-b") || Reaction1.equals("-c")) ) {
+		   					sentence("You can only Type -a or -b or -c"+newLine+ "Please Try Again");
+		   			Reaction1 = scanner.nextLine();
+		   	           }
+		   	           reactionToDoorClosing(Reaction1);
+		   		   }
+
+
+		              /**
+		   		 	 *	This function is used to inform the reader about the outcome of his choice to the door closing.
+		   		 	 *
+		   		 	 *	@param answer The answer of the reader.
+		   	         */
+		            static void reactionToDoorClosing(String answer){
+		   			 String newLineC = System.getProperty("line.separator");
+
+		                   if (answer.equals("-a")){
+		   				sentence("You close your eyes and open them again after a second.'What's happening?...'"
+		   				+newLineC+"but before you manage to complete your thoughts you see her.\nAn old woman in a black dress is standing right in front of you!"
+		   				+newLineC+"when you focus you can see her bloody deformed face\njust a breath away from yours and her pale white eyes staring into yours!"
+		   				+newLineC+"You try to scream but it's like there is no air in your lungs...\nThe last thing you can feel is your body going numb "
+		   				+newLineC+"and the last thing you hear is her terrifying voice...I HAVE FOUND YOU!"
+		   				+newLineC+"After a couple of days a relative visited your house\nbut he didn't find you there."
+		   				+newLineC+"No one ever found out what happened to that young,kind person\nliving in the house on the cliff..."
+		   				);}
+		   			else if (answer.equals("-b")){
+		   				 sentence("The door opens after a little push.You light a candle and you head downstairs."
+		   				 +newLineC+"You walk down the stairs and into the living room.\nAs you approach the kitchen you notice a movement on your right!"
+		   				 +newLineC+"You turn quickly and your heart loses a beat only to realise you are looking at your mirror."
+		   				 +newLineC+"'I am so stupid' you think.You turn around and open the door to the kitchen when suddenly you hear a cracking sound!"
+		   				 +newLineC+"You turn around slowly and you see the mirror,but this time it is cracked...How did this happen?"
+		   				 +newLineC+"How do you continue?"
+		                 +newLineC+"-a \tYou approach the mirror to see what caused the crack."
+		                 +newLineC+"-b \tOk thats it,you are getting out of this place!"
+		   				 );
+
+		   				  Scanner scan = new Scanner(System.in);
+		   				 		String Reaction2 = scan.nextLine();
+
+		   				 		while (!(Reaction2.equals("-a") || Reaction2.equals("-b")) ) {
+		   				 			sentence("You can only Type -a or -b"+newLineC+ "Please Try Again");
+		   				 			Reaction2 = scan.nextLine();
+		   	                   }
+		                       ReactionToCrackedMirror(Reaction2);
+		   				 }
+		   			else{
+		   				sentence("You don't know how much time passed since the door closed.\nA minute?Two minutes?A year?"
+		   				+newLineC+"The only thing you know for sure is you have to do something.\nThe moment you decide to move you hear an echoing whisper..."
+		   				+newLineC+"'I AM COMING FOR YOU...' a moment of silence and then footsteps behind the door!"
+		   				+newLineC+"-a \tScared like hell you jump out of the window."
+		                +newLineC+"-b \tYou remain still,you close your eyes and think 'It's not real...its not real...'"
+		   				);
+		   				             Scanner scan = new Scanner(System.in);
+		   							  String Reaction2 = scan.nextLine();
+
+		   								 		while (!(Reaction2.equals("-a") || Reaction2.equals("-b")) ) {
+		   								 			sentence("You can only Type -a or -b"+newLineC+ "Please Try Again");
+		   								 			Reaction2 = scan.nextLine();
+		   					                   }
+		                       ReactionToFootsteps(Reaction2,newLineC);
+		   			}
+
+		   		 }
+
+
+
+		                       /**
+		   					 *	This function continues the creepy story
+		   					 *
+		   					 *	@param answer The answer of the reader.
+		   	                 */
+		              static void ReactionToFootsteps(String answer,String newline4){
+		   			   if (answer.equals("-a")){
+		   			   sentence("The window breaks with a loud noise\nand you find yourself lying on the wet ground of your garden"
+		   			   +newline4+"You try to stand up but you feel a sharp pain from your leg."
+		   			   +newline4+"After gathering all your remaining strength you stand up and start running!"
+		   			   +newline4+"You run as fast as you can,trying to ignore your pain."
+		   			   +newline4+"As you run away from your house through the heavy rain\nyou could swear that you heard an old woman screaming but you won't stop."
+		   			   +newline4+"After some time you reach the nearby village."
+		   			   +newline4+"You stop,take a deep breath and relax.There are other people there,you are safe..."
+		   			   +newline4+".\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\nOr are you?");
+		   			   }
+		   			   else{
+		   			   sentence("Unfortunately it is real...\nthe door opens and you stand there staring at the terrifying"
+		   			   +newline4+"figure of an old woman in a black dress floating above the floor."
+		   			   +newline4+"As you are paralyzed with fear she gets close and touches your chest with her cold hand."
+		   			   +newline4+"You try to talk but the words refuse to come out."
+		   			   +newline4+"As you fall into the void you can hear her voice saying 'Your time has come...emprace the darkness'"
+		   			   );
+		   			      }
+		   		        }
+
+
+
+
+
+		   		            /**
+		   		 		 	 *	This function continues the creepy story
+		   		 		 	 *
+		   		 		 	 *	@param answer The answer of the reader.
+		   	                 */
+		   		 static void ReactionToCrackedMirror(String answer){
+		   		 String newLineD = System.getProperty("line.separator");
+
+		   		 if (answer.equals("-a")){
+		   		 				sentence("As you walk towards the mirror,you notice something very strange."
+		   						+newLineD+"Your reflection on the cracked mirror won't mimic your movement.\nActually it won't move at all!"
+		   						+newLineD+"It's just standing there...looking at a spot right over your left shoulder..."
+		   						+newLineD+"You slowly turn your head when you feel a cold hand grasping your neck."
+		   						+newLineD+"How do you react?"
+		   						+newLineD+"-a I run as fast as i can and  try to get out."
+		   						+newLineD+"-b I close my eyes and try to convince myself that everything is in my head"
+		   							);
+		   							Scanner scan1 = new Scanner(System.in);
+		   									String Reaction3 = scan1.nextLine();
+
+		   							while (!(Reaction3.equals("-a") || Reaction3.equals("-b")) ) {
+		   										sentence("You can only Type -a or -b"+newLineD+ "Please Try Again");
+		   											Reaction3 = scan1.nextLine();
+		   					                   }
+		                                reactionToUnsettlingEvent(Reaction3,newLineD);
+		   							}
+		   		 			else{
+		   				     happyEnd(newLineD);
+		   				    }
+
+		   		 }
+		                    /**
+		   				 *	This function continues the creepy story
+		   				 *
+		   				 *	@param answer The answer of the reader.
+		   	             */
+		            static void reactionToUnsettlingEvent(String answer,String newlineS){
+		   			 if (answer.equals("-a"))
+		   			 happyEnd(newlineS);
+		   			 else{
+		   				 sentence("You close your eyes but nothing changes."
+		   			     +newlineS+"You understand now that everything is real...but its too late."
+		   			     +newlineS+"You try to move but your body won't react to your will."
+		   			     +newlineS+"The last thing you manage to do is utter a question...'Who are you?'"
+		   			     +newlineS+"\n\n\nI am the fourth horseman...\n\n\n"
+		   					 );
+		   				 }
+		   			 }
+
+		   		 static void happyEnd(String newlineS){
+		   			 sentence("You run as fast as you can,practically breaking the entrance door."
+		   			 +newlineS+"As you run away from your house through the heavy rain\nyou could swear that you heard an old woman screaming but you won't stop."
+		   			 +newlineS+"After some time you reach the nearby village."
+		   			 +newlineS+"You stop,take a deep breath and relax.There are other people there,you are safe..."
+		   			 +newlineS+".\n.\n.\n.\n.\n.\n.\n.\n.\n.\n.\nOr are you?");
+		   			 }
+
+
 	/**
 	 * Function that is used to create an Adventure story.
 	 *
@@ -214,7 +384,7 @@ public class ExcitingStory {
 		String newLine = System.getProperty("line.separator");
 		Scanner scanner = new Scanner(System.in);
 		sentence("It was a dark and stormy night.");
-        sentence( newLine +"If you were terrified choose -a" + newLine + "If you enjoyed it, choose -b" + newLine + "If you want adventure, choose -c" + newLine + "If you want science fiction, choose -d" + newLine + "If you want a different story, choose -e" + newLine + "If you want a scary story, choose -f");
+        sentence( newLine +"If you were terrified choose -a" + newLine + "If you enjoyed it, choose -b" + newLine + "If you want adventure, choose -c" + newLine + "If you want science fiction, choose -d" + newLine + "If you want a different story, choose -e" + newLine + "If you want a scary story, choose -f"+ newLine + "If you want a creepy story, choose -g");
 
 
 		/* Initial choice of the reader among Happy, Horryfying, Adventure and Science Fiction Story */
@@ -222,8 +392,8 @@ public class ExcitingStory {
 		String m=scanner.nextLine();
 
 		/* A loop which force the user to insert the correct character (-a or -b or -c or -d) */
-		while (!(m.equals("-a") || m.equals("-b") || m.equals("-c") || m.equals("-d") || m.equals("-e") || m.equals("-f")) ) {
-	        	sentence("You can only Type -a or -b or -c or -d or -e or -f" +newLine+ "Please Try Again");
+		while (!(m.equals("-a") || m.equals("-b") || m.equals("-c") || m.equals("-d") || m.equals("-e") || m.equals("-f")|| m.equals("-g")) ) {
+	        	sentence("You can only Type -a or -b or -c or -d or -e or -f or -g" +newLine+ "Please Try Again");
 			m = scanner.nextLine();
 
                 }
@@ -241,5 +411,7 @@ public class ExcitingStory {
 		        telldifferentStory(newLine);
 		else if (m.equals("-f"))
 			tellScaryStory(newLine);
+		else if (m.equals("-g"))
+			tellCreepyStory(newLine);
 	}
 }
